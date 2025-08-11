a90dab7ca6535945bda0a06e6fafa3194e8b379d
Anderson Freires <dre.son36@gmail.com>
2025-08-11 20:32:21 -0300

    Class client

--- CustomerRegistration/src/main/java/com/br/dre/model/Client.java
+++ CustomerRegistration/src/main/java/com/br/dre/model/Client.java
@@ -12,8 +12,7 @@ public class Client {
 	@Id
 	@GeneratedValue(strategy= GenerationType.IDENTITY)
 	private Long id;
-	
-	
+		
 	private String name;
 	private String email;
 	private String telephone;
